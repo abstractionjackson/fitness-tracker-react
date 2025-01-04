336216b08ca01e999ba1a8b2df816c0852d5df8c
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2025-01-04 09:25:56 -0800

    update base name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://abstractionjackson.github.io/fitness-tracker/",
+  base: "https://abstractionjackson.github.io/fitness-tracker-react/",
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
